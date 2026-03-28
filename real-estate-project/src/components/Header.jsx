9084686361bf1fd24639a039cd99c6aab4f715f4
Nikhil Bobde <bobdenikhil53@gmail.com>
2026-03-28 10:57:27 +0530

    initial commit

---
+++ real-estate-project/src/components/Header.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+
+    
+
+    return (
+        <header className='bg-white shadow-md'>
+            <div className='flex justify-between items-center max-w-6xl mx-auto p-4'>
+
+                {/* LOGO */}
+                <Link to='/'>
+                    <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                        <span className='text-gray-500'>Liberty</span>
+                        <span className='text-slate-800'>Towers</span>
+                    </h1>
+                </Link>
+
+                {/* SEARCH BAR */}
+                <form className='bg-slate-100 p-2 rounded-lg flex items-center'>
+                    <input
+                        type="text"
+                        placeholder='Search...'
+                        className='bg-transparent focus:outline-none w-24 sm:w-64'
+                    />
+                    <button>
+                        <FaSearch className='text-slate-600' />
+                    </button>
+                </form>
+
+                {/* NAV LINKS */}
+                <ul className='flex gap-4 items-center'>
+
+                    <Link to='/'>
+                        <li className='hidden sm:inline text-slate-700 hover:underline'>
+                            Home
+                        </li>
+                    </Link>
+
+                    <Link to='/about'>
+                        <li className='hidden sm:inline text-slate-700 hover:underline'>
+                            About
+                        </li>
+                    </Link>
+
+                    <Link to='/profile'>
+                        {/* {currentUser ? (
+                            <img
+                                className='rounded-full h-8 w-8 object-cover'
+                                src={currentUser.avatar}
+                                alt='profile'
+                            />
+                        ) : (
+                            <li className='text-slate-700 hover:underline'>
+                                Sign in
+                            </li>
+                        )} */}
+                    </Link>
+
+                </ul>
+
+            </div>
+        </header>
+    )
+}
