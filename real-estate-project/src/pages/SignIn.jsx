1d4c0b6b8cf18b82a3cef26983b96c345997c5b0
Nikhil Bobde <bobdenikhil53@gmail.com>
2026-03-29 16:42:21 +0530

    add redux toolkit

--- real-estate-project/src/pages/SignIn.jsx
+++ real-estate-project/src/pages/SignIn.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { signInFailure, signInStart, signInSuccess } from '../redux/user/userSlice'
 
 export default function SingIn() {
-  const [formData, setFormData] = useState({})
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({}) ; 
+
+  const {loading , error} = useSelector((state) => state.user) ;
+
   const navigate = useNavigate();
+
+  const dispatch = useDispatch() ; 
   const handleChange = (e) => {
     setFormData(
       {
@@ -20,7 +25,8 @@ export default function SingIn() {
     e.preventDefault();
 
     try {
-      setLoading(true);
+      // setLoading(true);
+      dispatch(signInStart() ) ; 
 
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
@@ -32,19 +38,16 @@ export default function SingIn() {
 
       const data = await res.json();
 
-      if (!res.ok) {
-        setLoading(false);
-        setError(data.message);
+      if (data.success === false ) {
+        dispatch(signInFailure(data.message)) ;
         return;
       }
 
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data)) ; 
       navigate('/');
 
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message)) ; 
     }
   };
 
